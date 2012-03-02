4b7026126ec91a6c000d34e50b221f019f612d93
cadorn <christoph@christophdorn.com>
2012-03-02 13:51:51 -0800

    web worker example

--- examples/04-PlatformFeatures/03-BundlerMiddlewareBundleLoader/example/main.js
+++ examples/04-PlatformFeatures/03-BundlerMiddlewareBundleLoader/example/main.js
@@ -7,9 +7,26 @@ exports.main = function()
     {
         SUB_MODULE.main();
         
-        require.sandbox("sub-package.js", function(sandbox)
+        require.sandbox("./sub-package.js", function(sandbox)
         {
             sandbox.main();
         });
     });
+
+    // TODO: Get this working in tests. Could work with http://www.phantomjs.org/ ?
+    if (typeof Worker === "undefined") return;
+
+    var worker = new Worker("/bundles/worker-package.js");
+    
+    worker.onerror = function()
+    {
+        console.error.apply(null, arguments);
+    }
+
+    worker.onmessage = function(msg)
+    {
+        if (msg.data.type === "log") {
+            console.log(msg.data.data);
+        }
+    }
 }
